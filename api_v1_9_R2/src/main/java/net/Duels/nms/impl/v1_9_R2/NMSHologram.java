76af5325c3475b5eed3f0ca39d63a62f91d9ffc2
crafter23456 <64259198+crafter23456@users.noreply.github.com>
2022-02-19 20:57:02 +0100

    2

---
+++ api_v1_9_R2/src/main/java/net/Duels/nms/impl/v1_9_R2/NMSHologram.java
@@ -0,0 +1,73 @@
+package net.Duels.nms.impl.v1_9_R2;
+
+import net.Duels.nms.Hologram;
+import net.Duels.utility.ChatUtils;
+import net.minecraft.server.v1_9_R2.EntityArmorStand;
+import net.minecraft.server.v1_9_R2.PacketPlayOutEntityDestroy;
+import net.minecraft.server.v1_9_R2.PacketPlayOutEntityMetadata;
+import net.minecraft.server.v1_9_R2.PacketPlayOutSpawnEntityLiving;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_9_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_9_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class NMSHologram implements Hologram {
+    private EntityArmorStand armorStand;
+    private String text;
+    private Location location;
+
+    @Override
+    public void spawn(Location location, String text) {
+        this.location = location;
+        this.text = text;
+        (this.armorStand = new EntityArmorStand(((CraftWorld) location.getWorld()).getHandle())).setLocation(location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
+        this.armorStand.setCustomNameVisible(true);
+        this.armorStand.setGravity(false);
+        this.armorStand.setCustomName(ChatUtils.colorTranslate(text));
+        this.armorStand.setInvisible(true);
+    }
+
+    @Override
+    public void sendTo(Player... array) {
+        for (Player player : array) {
+            ((CraftPlayer) player).getHandle().playerConnection.sendPacket(new PacketPlayOutSpawnEntityLiving(this.armorStand));
+            this.update(array);
+        }
+    }
+
+    @Override
+    public void update(Player... players) {
+        for (Player player : players) {
+            ((CraftPlayer) player).getHandle().playerConnection.sendPacket(new PacketPlayOutEntityMetadata(this.armorStand.getId(), this.armorStand.getDataWatcher(), false));
+        }
+    }
+
+    @Override
+    public void remove(Player... array) {
+        for (int length = array.length, i = 0; i < length; ++i) {
+            ((CraftPlayer) array[i]).getHandle().playerConnection.sendPacket(new PacketPlayOutEntityDestroy(this.armorStand.getId()));
+        }
+    }
+
+    @Override
+    public void setArmorStandText(String s) {
+        this.armorStand.setCustomName(ChatUtils.colorTranslate(s));
+    }
+
+    public Location getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    @Override
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
